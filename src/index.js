b328cc173c0031d4e7a4627209b93cf1165beb0e
Zoe Yu <zoeyu97@gmail.com>
2019-04-20 14:06:30 -0400

    added rest of starterpack from SA4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import $ from 'jquery';
 import './style.scss';
 
-let num = 0;
-const ms = 1000;
-setInterval(
-  () => {
-    num += 1;
-    $('#main').html(`You've been on this page for ${num} seconds.`);
-  },
-  ms,
-);
+// let num = 0;
+// const ms = 1000;
+// setInterval(
+//   () => {
+//     num += 1;
+//     $('#main').html(`You've been on this page for ${num} seconds.`);
+//   },
+//   ms,
+// );
+
+const App = () => <div className="test">Help ME</div>;
+
+// const note = {
+//   title: 'testing',
+//   text: 'I is a note',
+//   x: 400,
+//   y: 12,
+//   zIndex: 26,
+// };
+
+// $('#main').html(note);
+
+
+ReactDOM.render(<App />, document.getElementById('main'));
